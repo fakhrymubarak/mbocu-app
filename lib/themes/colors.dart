898e1cad915d1286c28fe13c436963735bd2bea5
A. Muh. Fakhry Mubarak <fakhrymubarak@gmail.com>
2021-09-10 17:37:47 +0700

    refactor: import ScreenUtil library and set constant `colors` and `text_styles`

---
+++ lib/themes/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+const colorPrimary = Color(0xFFFFCA61);
+const colorBackground = Color(0xFFD4CCCC);
+const gradientPrimary = LinearGradient(
+  begin: Alignment.topCenter,
+  end: Alignment.bottomCenter,
+  colors: [Color(0xFFFFCA61), Color(0xffEB9C00)],
+);
+
+const colorBlack = Color(0xFF383636);
+const colorGreyDark = Color(0xFF848484);
+const colorGreyLight = Color(0xFFF1F0F0);
---
+++ lib/themes/text_styles.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mbocu_app/themes/colors.dart';
+
+TextStyle tsTitle = GoogleFonts.lato(
+    fontSize: 40, fontWeight: FontWeight.w700, color: colorBlack);
+
+TextStyle tsHeading1 = GoogleFonts.lato(
+    fontSize: 20, fontWeight: FontWeight.w700, color: colorBlack);
+
+TextStyle tsHeading2 = GoogleFonts.lato(
+    fontSize: 15, fontWeight: FontWeight.w600, color: colorBlack);
+
+TextStyle tsRegular = GoogleFonts.lato(
+    fontSize: 12, fontWeight: FontWeight.w400, color: colorBlack);
+
+TextStyle tsRegularBold = GoogleFonts.lato(
+    fontSize: 12, fontWeight: FontWeight.w700, color: colorBlack);
+
+TextStyle tsSmall = GoogleFonts.lato(
+    fontSize: 10, fontWeight: FontWeight.w400, color: colorBlack);
