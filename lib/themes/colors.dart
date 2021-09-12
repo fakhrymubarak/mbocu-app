b6f6d6219f43e00881536a3fc8a45a2c9e9f28aa
A. Muh. Fakhry Mubarak <fakhrymubarak@gmail.com>
2021-09-12 13:11:55 +0700

    refactor: add white colors and remove background primary

--- lib/themes/colors.dart
+++ lib/themes/colors.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 
 const colorPrimary = Color(0xFFFFCA61);
-const colorBackground = Color(0xFFD4CCCC);
 const gradientPrimary = LinearGradient(
   begin: Alignment.topCenter,
   end: Alignment.bottomCenter,
@@ -9,5 +8,6 @@ const gradientPrimary = LinearGradient(
 );
 
 const colorBlack = Color(0xFF383636);
+const colorWhite = Color(0xFFFFFFFF);
 const colorGreyDark = Color(0xFF848484);
 const colorGreyLight = Color(0xFFF1F0F0);
